616eb774bb3cf2d239fa96c3327293cd6bd0cc5d
Neha Soni <nehasoni6001@gmail.com>
2023-01-13 22:44:27 +0530

    hgfh

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { Container } from '@material-ui/core';
+import { BrowserRouter, Route } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header/Header';
+import LabelBottomNavigation from './components/MainNav';
+import Trending from './Pages/Trending/Trending';
+import Movies from './Pages/Movies/Movies';
+import Series from './Pages/Series/Series';
+import Search from './Pages/Search/Search';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Header />
+    <div className="App">
+      <Container>
+          <Route>
+          <Route path="/" component={Trending} exact />
+            <Route path="/movies" component={Movies} />
+            <Route path="/series" component={Series} />
+            <Route path="/search" component={Search} />
+          </Route>
+      </Container>
+    </div>
+    <LabelBottomNavigation />
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import "./Header.css";
+
+const Header = () => {
+    return (
+      <span className="header"  onClick={() => window.scroll(0, 0)}>
+        🎬 Entertainment Hub 🎥
+      </span>
+    );
+  };
+  
+  export default Header;
+  
